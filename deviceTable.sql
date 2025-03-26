d458a52e09702e81a208d146287897ce94ef25b5
pcRonem <peter.ronemous@gmail.com>
2025-03-25 21:21:37 -0500

    Update deviceTable.sql

--- deviceTable.sql
+++ deviceTable.sql
@@ -4,17 +4,21 @@ create table device (
   serialNo varchar(30) NOT NULL, -- all computers come with a serial number
   PONo varchar(30), -- all computers come in shipments or are logged for tax purposes
   deviceType varchar(40) NOT NULL,
-  isActive binary(1), -- 0 for no, 1 for yes
-  isFunctional binary(1),  -- 0 for no, 1 for yes
-  isDamaged binary(1), -- 0 for no, 1 for yes
+  isActive binary(1) NOT NULL DEFAULT'0', -- 0 for no, 1 for yes
+  isDamaged binary(1) NOT NULL DEFAULT'0', -- 0 for no, 1 for yes
   damageComment varchar(280),
-  currentUser varchar(26), -- pcr0305@pvamu.edu, adminFaculty, Dr. Genius MD, custodianSteve
-  loginPermissions varchar(10), -- admin, student, faculty, maintenance
-  isStolen binary(1),  -- 0 for no, 1 for yes
-  isLost binary(1), -- 0 for no, 1 for yes
+  currentUser varchar(26) NOT NULL DEFAULT'none', -- pcr0305@pvamu.edu, adminFaculty, Dr. Genius MD, custodianSteve, none
+  loginPermissions varchar(10) NOT NULL DEFAULT'none', -- admin, student, faculty, maintenance, none
+  isStolen binary(1) NOT NULL DEFAULT'0',  -- 0 for no, 1 for yes
+  isLost binary(1) NOT NULL DEFAULT'0', -- 0 for no, 1 for yes
   otherComments varchar(280),
-  isFieldReady binary(1), -- 0 for no, 1 for yes
+  isFieldReady binary(1) NOT NULL DEFAULT'0', -- 0 for no, 1 for yes
   PRIMARY KEY (assetNo)
 );
 
---
+-- function sync(checks isDamaged, isStolen, isLost for all negatives)
+--              (checks PONo is not null)
+--                 before returning true and setts isFieldReady to 1)
+
+
+-- function
