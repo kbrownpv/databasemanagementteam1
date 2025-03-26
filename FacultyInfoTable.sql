d1b3f6f01222d39ad968f7ef69f16ca77c3f1133
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-03-26 10:22:48 -0500

    Update FacultyInfoTable.sql

--- FacultyInfoTable.sql
+++ FacultyInfoTable.sql
@@ -1,1 +1,11 @@
 
+CREATE TABLE FacultyInfo (
+    FacultyID INT PRIMARY KEY,
+    FacultyFName VARCHAR(100),
+    FacultyBal DECIMAL(6,2),
+    AdminComments TEXT,
+    OutstandingBal DECIMAL(6,2),
+    numDamages INT,
+    canRent BOOLEAN,
+    currentDevice VARCHAR(50)
+);
