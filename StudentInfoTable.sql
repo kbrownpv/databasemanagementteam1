9d4668ddb7b9ee37f95defc6a1faa23f90452bbb
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-03-25 21:25:11 -0500

    Update StudentInfoTable.sql

--- StudentInfoTable.sql
+++ StudentInfoTable.sql
@@ -1,1 +1,13 @@
+CREATE TABLE StudentInfo (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(100),
+    StudentBal DECIMAL(6,2),
+    AdminComments TEXT,
+    OutstandingBal DECIMAL(6,2),
+    isRegistered BOOLEAN,
+    isAttending BOOLEAN,
+    numDamages INT,
+    canRent BOOLEAN,
+    currentDevice VARCHAR(50)
+);
 
