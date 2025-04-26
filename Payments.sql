974e42eb40a3b90f0a6a35601cb3b2a9b46e3602
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-04-26 12:19:47 -0500

    Update Payments.sql

--- Payments.sql
+++ Payments.sql
@@ -39,21 +39,3 @@ CREATE TABLE PaymentLog (
 );
 
 
--- TRIGGER: Log New Payments
--- Automatically logs every new payment inserted into the Payments table
-
-DELIMITER $$
-
-CREATE TRIGGER log_new_payment
-AFTER INSERT ON Payments
-FOR EACH ROW
-BEGIN
-  INSERT INTO PaymentLog (PaymentID, LogDate, LogNote)
-  VALUES (
-    NEW.PaymentID,
-    NOW(),
-    CONCAT('New payment of $', NEW.TotalPaid, ' added for User ', NEW.UserID)
-  );
-END$$
-
-DELIMITER ;
