fa45ad7adaa904165a77b4de1c92a3dee96f75f9
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-04-28 10:25:13 -0500

    Update query_paymentswithremainingbalance.sql

--- OverDue_SecurityFolder/query_paymentswithremainingbalance.sql
+++ OverDue_SecurityFolder/query_paymentswithremainingbalance.sql
@@ -1,3 +1,12 @@
-SELECT PaymentID, UserID, TotalPaid, RemainingBal
-FROM Payments
-WHERE RemainingBal > 0;
+SELECT 
+  p.PaymentID,
+  p.UserID,
+  d.DeviceType,
+  p.TotalPaid,
+  p.RemainingBal
+FROM 
+  Payments p
+JOIN 
+  Device d ON p.DeviceID = d.SerialNo
+WHERE 
+  p.RemainingBal > 0;
