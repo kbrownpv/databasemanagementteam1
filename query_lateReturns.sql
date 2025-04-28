73f3a7e157fab3768057b2b355fffc33c29bc911
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-04-28 10:37:09 -0500

    Update query_lateReturns.sql

--- query_lateReturns.sql
+++ query_lateReturns.sql
@@ -1,14 +1,11 @@
--- Query: List rentals that were returned late
 SELECT 
-  r.RentalNum,
-  r.RentalDate,
-  r.RentalDue,
-  r.ReturnDate,
-  d.DeviceType,
-  d.SerialNo
-FROM 
-  Rentals r
-JOIN 
-  Device d ON r.DeviceID = d.SerialNo
-WHERE 
-  r.ReturnDate > r.RentalDue;
+    r.RentalNum,
+    r.DeviceID,
+    r.StudentID,
+    r.RentalDate,
+    r.RentalDue,
+    r.ReturnDate
+FROM Rentals r
+JOIN Device d ON r.DeviceID = d.SerialNo
+WHERE r.ReturnDate IS NOT NULL
+AND r.ReturnDate > r.RentalDue;
