5f2eef3665883974be7fa527b25e37b7d18f19fd
Kendall B <158317605+kbrownpv@users.noreply.github.com>
2025-04-28 02:25:37 -0500

    Update query_totalPaidPerStudent.sql

--- query_totalPaidPerStudent.sql
+++ query_totalPaidPerStudent.sql
@@ -2,13 +2,16 @@
 -- Query joins the Payments table with the StudentInfo table to display each
 -- student's name and the total amount they've paid so far.
 SELECT 
-  s.studentID,
-  s.firstName,
-  s.lastName,
-  SUM(p.TotalPaid) AS TotalPaid
+    s.studentID,
+    s.firstName,
+    s.lastName,
+    SUM(p.TotalPaid) AS TotalPaid
 FROM 
-  Payments p
+    Payments p
 JOIN 
-  StudentInfo s ON p.UserID = s.studentID
+    StudentInfo s ON p.UserID = s.studentID
+WHERE
+    p.UserID IS NOT NULL
 GROUP BY 
-  s.studentID, s.firstName, s.lastName;
+    s.studentID, s.firstName, s.lastName;
+
